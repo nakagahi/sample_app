02e6b0205966c030e0f99e27a5af20b42db96738
nakagawahiroki <iwbb1022@icloud.com>
2020-08-03 02:51:46 +0900

    finish chap07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,43 @@
 class UsersController < ApplicationController
+
+  # GET users/new
   def new
+    @user = User.new
   end
+
+  # GET users/show/:id
+  def show
+
+    @user = User.find(params[:id])
+
+  end
+
+
+  def create
+
+    @user =User.create(user_params)
+
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # redirect_to user_path(@user)
+      # redirect_to user_path(@user)
+      # redirect_to user_path(1)
+                     # => /users/1
+
+    else
+
+      render 'new'
+
+    end
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+
 end
